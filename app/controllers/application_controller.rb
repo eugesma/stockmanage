e897bab250956fb77013c9636ad274f95fd53be4
BasHud <eugesma@gmail.com>
2020-01-07 11:39:30 -0300

    Trying to fix heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
 
-  # You want to get exceptions in development, but not in production.
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from ActionController::UnknownController, with: -> { render_404  }
-    rescue_from ActiveRecord::RecordNotFound,        with: -> { render_404  }
-  end
-
   def render_404
     respond_to do |format|
       format.html { render template: 'errors/not_found', status: 404 }
