07b99ae743118d2c331b1907311b5d8545cec89d
BasHud <eugesma@gmail.com>
2020-01-09 21:34:49 -0300

    Add roles

---
+++ app/policies/store_policy.rb
@@ -0,0 +1,37 @@
+class StorePolicy < ApplicationPolicy
+  def index?
+    user.has_any_role?(:admin)
+  end
+
+  def show?
+    user.has_any_role?(:admin)
+  end
+
+  def create?
+    user.has_any_role?(:admin)
+  end
+
+  def new?
+    create?
+  end
+
+  def new_factory?
+    create?
+  end
+
+  def update?
+    user.has_any_role?(:admin)
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user.has_any_role?(:admin, :vendedor)
+  end
+
+  def delete?
+    self.destroy?
+  end
+end
