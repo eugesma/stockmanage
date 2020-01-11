c47b1be093a6b0f28f90d13a749566fa32509a37
paul <paul.18.sma@gmail.com>
2020-01-11 00:44:34 -0300

    adds products abm

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_111145) do
+ActiveRecord::Schema.define(version: 2020_01_09_133732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.decimal "cost_price"
+    t.decimal "sale_price"
+    t.decimal "store_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
